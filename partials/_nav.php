0b89dce0c1cec3a87125f4f1a26803c47af89ec5
Mukesh Lilawat <143742837+mukeshlilawat1@users.noreply.github.com>
2025-07-16 16:42:26 +0530

    complete login system

--- partials/_nav.php
+++ partials/_nav.php
@@ -1,4 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+<?php
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    $loggedin = true;
+}else {
+    $loggedin = false;
+}
+
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/LoginSystem">ISecure</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,22 +16,30 @@
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="/LoginSystem/welcome.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
+            </li>';
+
+if (!$loggedin) {
+    # code...
+    echo '<li class="nav-item">
                 <a class="nav-link" href="/LoginSystem/login.php">Login</a>
             </li>
-
             <li class="nav-item">
-                <a class="nav-link" href="/LoginSystem/signup.php">Signup</a>
-            </li>
+                        <a class="nav-link" href="/LoginSystem/signup.php">Signup</a>
+            </li>';
+}
 
-            <li class="nav-item">
+if ($loggedin) {
+    # code...
+    echo '<li class="nav-item">
                 <a class="nav-link" href="/LoginSystem/logout.php">LogOut</a>
-            </li>
-        </ul>
+            </li>';
+}
+
+
+echo '</ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
-</nav>
+</nav>'; ?>
